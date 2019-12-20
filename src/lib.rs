aec34b14aa757d2c23cb34ed0509b62f6f0e9ab9
Pat Hickey <phickey@fastly.com>
2019-12-19 16:47:02 -0800

    well, we can find stuff that witx-headers doesnt support

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use tempfile;
+#[cfg(test)]
+use wasi_headers;
+
 mod generate_doc;
 mod render;
 
@@ -5,16 +9,57 @@ pub use generate_doc::{
     GenDoc, GenEnum, GenFlags, GenHandle, GenStruct, GenType, GenTypeRef, GenUnion, Limits,
 };
 
+pub fn check_header(header: &str) {
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::PathBuf;
+    use std::process::Command;
+    use tempfile::TempDir;
+
+    let tempdir = TempDir::new().expect("create tempdir");
+    let mut f = File::create(tempdir.path().join("test.h")).expect("create test.h");
+    f.write_all(header.as_bytes()).expect("write test.h");
+
+    let wasi_sdk = PathBuf::from("/opt/wasi-sdk/");
+    //PathBuf::from(std::env::var("WASI_SDK").unwrap_or_else(|_| "/opt/wasi-sdk".to_string()));
+
+    let run_clang = Command::new(wasi_sdk.join("bin").join("clang"))
+        .arg("-c")
+        .arg(tempdir.path().join("test.h"))
+        .arg("-o")
+        .arg(tempdir.path().join("test.o"))
+        .output()
+        .expect("run wasi-sdk clang");
+    if !run_clang.status.success() {
+        panic!(
+            "clang rejected header: {}\n{}",
+            header,
+            String::from_utf8_lossy(&run_clang.stderr)
+        );
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{GenDoc, Limits};
+    use super::{check_header, GenDoc, Limits};
     use proptest::proptest;
     proptest! {
         #[test]
-        fn generate_witx(gen_doc in GenDoc::strat(&Limits::default())) {
+        fn generate_witx_spec(gen_doc in GenDoc::strat(&Limits::default())) {
             let gen_syntax = format!("{}", gen_doc);
             println!("{}", gen_syntax);
             witx::parse(&gen_syntax).unwrap();
+        }
+
+
+        #[test]
+        fn generate_witx_header(gen_doc in GenDoc::strat(&Limits::default())) {
+            let gen_syntax = format!("{}", gen_doc);
+            println!("{}", gen_syntax);
+            let doc = witx::parse(&gen_syntax).unwrap();
+            let header = wasi_headers::to_c_header(&doc, "wasi-test generated");
+            check_header(&header);
+
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use proptest::prelude::*;
 use witx;
 use witx_test::*;
+use wasi_headers;
 
 fn main() {
     let mut runner = prop::test_runner::TestRunner::default();
     let limits = Limits::default();
     let gen_doc = GenDoc::strat(&limits)
         .new_tree(&mut runner)
-        .unwrap()
+        .expect("generate doc tree")
         .current();
 
     let gen_syntax = format!("{}", gen_doc);
     println!("{}", gen_syntax);
 
-    witx::parse(&gen_syntax).unwrap();
+    let doc = witx::parse(&gen_syntax).expect("parse doc");
+
+    let header = wasi_headers::to_c_header(&doc, "wasi-test generated");
+
+    check_header(&header);
 }
